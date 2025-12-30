ff08cb7c1b1729ca8ba37a1006055b8ef3de817c
CPU256 <109115029+CPU256@users.noreply.github.com>
2025-12-30 14:25:41 -0500

    Worked on aimer, comments for fixes after you finish college apps. + Built-in timer

--- aimer.py
+++ aimer.py
@@ -8,15 +8,24 @@ from pynput.mouse import Listener
 
 clicked_positions = []
 click_count = 0
-def start_clicking(targets: int):
+#31, not 30 for hbenchmark
+number_of_targets = 0
+
+def start_clicking(targets):
     path = r'C:\Users\SHU\.vscode\auto_stuff\target.png'
     global clicked_positions
-    #God Python for loops are inhumane
-    aiming_region = [value for coordinate in clicked_positions for value in coordinate]
+    aiming_region = []
+    for coordinate in clicked_positions:
+        for value in coordinate:
+            aiming_region.append(value)
+
     for i in range(targets):
-        target_location = pyautogui.locateOnScreen(path,grayscale=True,region=aiming_region)
+        #for future  me, this can go faster if you use other imports like mss to find targets rather than pyautogui
+        #current speed is about 300-500 avg ms right now
+        #maybe take the screenshot
+        target_location = pyautogui.locateOnScreen(path, grayscale=True, region=aiming_region)
         pyautogui.leftClick(target_location)
-    print("Clicking Finished")
+    print("Done")
 
 
 
@@ -24,6 +33,7 @@ def calibrate():
     with mouse.Listener(on_click=on_click) as listener:
         listener.join()
 
+#copy-pasted from wordlebot, so there are definitely ways to make this cleaner
 def on_click(x, y, button, pressed):
     global click_count
     global clicked_positions
@@ -36,6 +46,7 @@ def on_click(x, y, button, pressed):
 def start():
     root = tk.Tk()
     root.withdraw()
+    global number_of_targets
     number_of_targets = tk.simpledialog.askinteger(title="Input", prompt="Please enter the number of targets you want to click")
     response = messagebox.askyesno("Confirm", "Start?")
     if response and number_of_targets:
@@ -55,4 +66,10 @@ def open_calibration_prompt():
 
 open_calibration_prompt()
 print("Calibration Done")
+start_time = time.time()
 start()
+total_time = time.time()-start_time
+print("Total time = " + str(total_time))
+print("Avg Time = " + str(total_time/number_of_targets))
+
+
